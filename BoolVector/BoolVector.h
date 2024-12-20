38aebf1f023f1841056be42a0dd67cdc72620cc8
Zahar <Zahar@DESKTOP-0T3OVPN.local>
2024-12-21 01:48:54 +0700

    Описан класс BoolVector с 4-мя конструкторами, всеми методами и перегрузками, а также дочерний класс Rank, нужный для работы с конкретными битами булевого вектора

---
+++ BoolVector/BoolVector.h
@@ -0,0 +1,67 @@
+#pragma once
+
+class BoolVector
+
+{
+public:
+    class Rank;
+
+public:
+    using Cell = unsigned char;
+    static const int CellSize = 8; // sizeof(Cell) * 8
+    BoolVector(int length = CellSize);
+    BoolVector(int length, bool value); 
+    BoolVector(const BoolVector& other);
+    ~BoolVector();
+    int length() const;//
+    void swap(BoolVector& other);//
+    bool bitValue(int index) const;
+    void setBitValue(int index, bool value);
+    void invert();
+    void invert(int index);
+    void setBits(int index, int count, bool value);
+    void setAllBits(bool value);
+    int weight() const;
+
+
+    BoolVector& operator=(const BoolVector& other);
+    BoolVector operator&(const BoolVector& other) const;
+    BoolVector& operator&=(const BoolVector& other);
+    BoolVector operator|(const BoolVector& other) const;
+    BoolVector& operator|=(const BoolVector& other);
+    BoolVector operator^(const BoolVector& other) const;
+    BoolVector& operator^=(const BoolVector& other);
+    BoolVector operator~() const;
+    BoolVector operator<<(int shift) const;
+    BoolVector& operator<<=(int shift);
+    BoolVector operator>>(int shift) const;
+    BoolVector& operator>>=(int shift);
+    friend std::ostream& operator<<(std::ostream& os, const BoolVector& bv);
+    friend std::istream& operator>>(std::istream& is, BoolVector& bv);
+    Rank operator[](int index);
+
+
+private:
+    int _excessRankCount() const;
+    static Cell _mask(int index);
+
+private:
+    Cell* m_cells = nullptr;
+    int m_cellCount = 0;
+    int m_length = 0;
+};
+
+
+class BoolVector::Rank
+{
+public:
+    Rank() = default;
+    Rank(Cell* cell, Cell mask);
+    Rank& operator=(const Rank& other);
+    Rank& operator=(bool value);
+    operator bool() const;
+
+private:
+    Cell* m_cell = nullptr;
+    Cell m_mask = 0;
+};
